faf090f7f00dda068cf8d9e4afceeb210081db8c
IrregularPorygon <IrregularPorygon@gmail.com>
2016-03-21 22:42:57 -0400

    Initial Commit

---
+++ Rainmeter_Shortcut_Creator/Form1.Designer.vb
@@ -0,0 +1,221 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ShortcutCreator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ShortcutCreator))
+        Me.PathLabel = New System.Windows.Forms.Label()
+        Me.AppPathTextBox = New System.Windows.Forms.TextBox()
+        Me.AppPathBrowse = New System.Windows.Forms.Button()
+        Me.GenDescLabel = New System.Windows.Forms.Label()
+        Me.ImgPathLabel = New System.Windows.Forms.Label()
+        Me.ImgPathTextBox = New System.Windows.Forms.TextBox()
+        Me.ImgPathBrowse = New System.Windows.Forms.Button()
+        Me.BackgroundCheck = New System.Windows.Forms.CheckBox()
+        Me.BackImageLabel = New System.Windows.Forms.Label()
+        Me.BackimageTextBox = New System.Windows.Forms.TextBox()
+        Me.BackImageBrowse = New System.Windows.Forms.Button()
+        Me.MonitorCheck = New System.Windows.Forms.CheckBox()
+        Me.ExitAbility = New System.Windows.Forms.CheckBox()
+        Me.Finished = New System.Windows.Forms.Button()
+        Me.Cancel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'PathLabel
+        '
+        Me.PathLabel.AutoSize = True
+        Me.PathLabel.Location = New System.Drawing.Point(12, 36)
+        Me.PathLabel.Name = "PathLabel"
+        Me.PathLabel.Size = New System.Drawing.Size(87, 13)
+        Me.PathLabel.TabIndex = 0
+        Me.PathLabel.Text = "Application Path:"
+        '
+        'AppPathTextBox
+        '
+        Me.AppPathTextBox.Location = New System.Drawing.Point(105, 33)
+        Me.AppPathTextBox.Name = "AppPathTextBox"
+        Me.AppPathTextBox.Size = New System.Drawing.Size(206, 20)
+        Me.AppPathTextBox.TabIndex = 1
+        '
+        'AppPathBrowse
+        '
+        Me.AppPathBrowse.Location = New System.Drawing.Point(317, 31)
+        Me.AppPathBrowse.Name = "AppPathBrowse"
+        Me.AppPathBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.AppPathBrowse.TabIndex = 2
+        Me.AppPathBrowse.Text = "Browse"
+        Me.AppPathBrowse.UseVisualStyleBackColor = True
+        '
+        'GenDescLabel
+        '
+        Me.GenDescLabel.AutoSize = True
+        Me.GenDescLabel.Location = New System.Drawing.Point(5, 9)
+        Me.GenDescLabel.Name = "GenDescLabel"
+        Me.GenDescLabel.Size = New System.Drawing.Size(306, 13)
+        Me.GenDescLabel.TabIndex = 3
+        Me.GenDescLabel.Text = "Create a Rainmeter Launcher Skin with the following properties:"
+        '
+        'ImgPathLabel
+        '
+        Me.ImgPathLabel.AutoSize = True
+        Me.ImgPathLabel.Location = New System.Drawing.Point(12, 65)
+        Me.ImgPathLabel.Name = "ImgPathLabel"
+        Me.ImgPathLabel.Size = New System.Drawing.Size(64, 13)
+        Me.ImgPathLabel.TabIndex = 4
+        Me.ImgPathLabel.Text = "Image Path:"
+        '
+        'ImgPathTextBox
+        '
+        Me.ImgPathTextBox.Location = New System.Drawing.Point(82, 62)
+        Me.ImgPathTextBox.Name = "ImgPathTextBox"
+        Me.ImgPathTextBox.Size = New System.Drawing.Size(229, 20)
+        Me.ImgPathTextBox.TabIndex = 5
+        '
+        'ImgPathBrowse
+        '
+        Me.ImgPathBrowse.Location = New System.Drawing.Point(317, 60)
+        Me.ImgPathBrowse.Name = "ImgPathBrowse"
+        Me.ImgPathBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.ImgPathBrowse.TabIndex = 6
+        Me.ImgPathBrowse.Text = "Browse"
+        Me.ImgPathBrowse.UseVisualStyleBackColor = True
+        '
+        'BackgroundCheck
+        '
+        Me.BackgroundCheck.AutoSize = True
+        Me.BackgroundCheck.Location = New System.Drawing.Point(8, 93)
+        Me.BackgroundCheck.Name = "BackgroundCheck"
+        Me.BackgroundCheck.Size = New System.Drawing.Size(202, 17)
+        Me.BackgroundCheck.TabIndex = 7
+        Me.BackgroundCheck.Text = "Display Background Image On Hover"
+        Me.BackgroundCheck.UseVisualStyleBackColor = True
+        '
+        'BackImageLabel
+        '
+        Me.BackImageLabel.AutoSize = True
+        Me.BackImageLabel.Location = New System.Drawing.Point(12, 119)
+        Me.BackImageLabel.Name = "BackImageLabel"
+        Me.BackImageLabel.Size = New System.Drawing.Size(96, 13)
+        Me.BackImageLabel.TabIndex = 8
+        Me.BackImageLabel.Text = "Background Path: "
+        '
+        'BackimageTextBox
+        '
+        Me.BackimageTextBox.Enabled = False
+        Me.BackimageTextBox.Location = New System.Drawing.Point(105, 116)
+        Me.BackimageTextBox.Name = "BackimageTextBox"
+        Me.BackimageTextBox.Size = New System.Drawing.Size(206, 20)
+        Me.BackimageTextBox.TabIndex = 9
+        '
+        'BackImageBrowse
+        '
+        Me.BackImageBrowse.Enabled = False
+        Me.BackImageBrowse.Location = New System.Drawing.Point(317, 114)
+        Me.BackImageBrowse.Name = "BackImageBrowse"
+        Me.BackImageBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.BackImageBrowse.TabIndex = 10
+        Me.BackImageBrowse.Text = "Browse"
+        Me.BackImageBrowse.UseVisualStyleBackColor = True
+        '
+        'MonitorCheck
+        '
+        Me.MonitorCheck.AutoSize = True
+        Me.MonitorCheck.Location = New System.Drawing.Point(7, 143)
+        Me.MonitorCheck.Name = "MonitorCheck"
+        Me.MonitorCheck.Size = New System.Drawing.Size(385, 17)
+        Me.MonitorCheck.TabIndex = 11
+        Me.MonitorCheck.Text = "Monitor Application Process (Greys out the icon if the program is not running)"
+        Me.MonitorCheck.UseVisualStyleBackColor = True
+        '
+        'ExitAbility
+        '
+        Me.ExitAbility.AutoSize = True
+        Me.ExitAbility.Enabled = False
+        Me.ExitAbility.Location = New System.Drawing.Point(15, 166)
+        Me.ExitAbility.Name = "ExitAbility"
+        Me.ExitAbility.Size = New System.Drawing.Size(362, 17)
+        Me.ExitAbility.TabIndex = 12
+        Me.ExitAbility.Text = "Allow Application Exiting (Closes the application if clicked while running)"
+        Me.ExitAbility.UseVisualStyleBackColor = True
+        '
+        'Finished
+        '
+        Me.Finished.Location = New System.Drawing.Point(259, 214)
+        Me.Finished.Name = "Finished"
+        Me.Finished.Size = New System.Drawing.Size(133, 23)
+        Me.Finished.TabIndex = 14
+        Me.Finished.Text = "Create the Shortcut!"
+        Me.Finished.UseVisualStyleBackColor = True
+        '
+        'Cancel
+        '
+        Me.Cancel.Location = New System.Drawing.Point(8, 214)
+        Me.Cancel.Name = "Cancel"
+        Me.Cancel.Size = New System.Drawing.Size(75, 23)
+        Me.Cancel.TabIndex = 15
+        Me.Cancel.Text = "Cancel"
+        Me.Cancel.UseVisualStyleBackColor = True
+        '
+        'ShortcutCreator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(404, 249)
+        Me.Controls.Add(Me.Cancel)
+        Me.Controls.Add(Me.Finished)
+        Me.Controls.Add(Me.ExitAbility)
+        Me.Controls.Add(Me.MonitorCheck)
+        Me.Controls.Add(Me.BackImageBrowse)
+        Me.Controls.Add(Me.BackimageTextBox)
+        Me.Controls.Add(Me.BackImageLabel)
+        Me.Controls.Add(Me.BackgroundCheck)
+        Me.Controls.Add(Me.ImgPathBrowse)
+        Me.Controls.Add(Me.ImgPathTextBox)
+        Me.Controls.Add(Me.ImgPathLabel)
+        Me.Controls.Add(Me.GenDescLabel)
+        Me.Controls.Add(Me.AppPathBrowse)
+        Me.Controls.Add(Me.AppPathTextBox)
+        Me.Controls.Add(Me.PathLabel)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "ShortcutCreator"
+        Me.Text = "Rainmeter Shortcut Creator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PathLabel As System.Windows.Forms.Label
+    Friend WithEvents AppPathTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents AppPathBrowse As System.Windows.Forms.Button
+    Friend WithEvents GenDescLabel As System.Windows.Forms.Label
+    Friend WithEvents ImgPathLabel As System.Windows.Forms.Label
+    Friend WithEvents ImgPathTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents ImgPathBrowse As System.Windows.Forms.Button
+    Friend WithEvents BackgroundCheck As System.Windows.Forms.CheckBox
+    Friend WithEvents BackImageLabel As System.Windows.Forms.Label
+    Friend WithEvents BackimageTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents BackImageBrowse As System.Windows.Forms.Button
+    Friend WithEvents MonitorCheck As System.Windows.Forms.CheckBox
+    Friend WithEvents ExitAbility As System.Windows.Forms.CheckBox
+    Friend WithEvents Finished As System.Windows.Forms.Button
+    Friend WithEvents Cancel As System.Windows.Forms.Button
+
+End Class
---
+++ Rainmeter_Shortcut_Creator/Form1.vb
@@ -0,0 +1,213 @@
+﻿Imports System.IO
+
+Public Class ShortcutCreator
+
+    Private Sub AppPathBrowse_Click(sender As Object, e As EventArgs) Handles AppPathBrowse.Click
+        Dim dialog As New OpenFileDialog()
+        If DialogResult.OK = dialog.ShowDialog Then
+            AppPathTextBox.Text = dialog.FileName
+        End If
+    End Sub
+
+    Private Sub ImgPathBrowse_Click(sender As Object, e As EventArgs) Handles ImgPathBrowse.Click
+        Dim dialog As New OpenFileDialog()
+        If DialogResult.OK = dialog.ShowDialog Then
+            ImgPathTextBox.Text = dialog.FileName
+        End If
+    End Sub
+
+    Private Sub BackImageBrowse_Click(sender As Object, e As EventArgs) Handles BackImageBrowse.Click
+        Dim dialog As New OpenFileDialog()
+        If DialogResult.OK = dialog.ShowDialog Then
+            BackimageTextBox.Text = dialog.FileName
+        End If
+
+    End Sub
+
+    Private Sub BackgroundCheck_CheckedChanged(sender As Object, e As EventArgs) Handles BackgroundCheck.CheckedChanged
+        BackimageTextBox.Enabled = BackgroundCheck.Checked
+        BackImageBrowse.Enabled = BackgroundCheck.Checked
+
+        If BackgroundCheck.Checked = False Then
+            BackimageTextBox.Text = ""
+        End If
+    End Sub
+
+    Private Sub MonitorCheck_CheckedChanged(sender As Object, e As EventArgs) Handles MonitorCheck.CheckedChanged
+        ExitAbility.Enabled = MonitorCheck.Checked
+        If MonitorCheck.Checked = False Then
+            ExitAbility.Checked = MonitorCheck.Checked
+        End If
+    End Sub
+
+    Private Sub Finished_Click(sender As Object, e As EventArgs) Handles Finished.Click
+        If AppPathTextBox.Text = "" Or ImgPathTextBox.Text = "" Then
+            MsgBox("Please fill in both the application path and the image path")
+        Else
+            If BackgroundCheck.Checked = True And BackimageTextBox.Text = "" Then
+                MsgBox("Please fill in the background image path")
+            Else
+                CreateStartTask()
+                CreateStopTask()
+                BuildIniFile()
+                Close()
+            End If
+        End If
+    End Sub
+
+    Private Sub Cancel_Click(sender As Object, e As EventArgs) Handles Cancel.Click
+        Close()
+    End Sub
+
+    ' My subs (yummy)
+
+    Private Function GetProcessName() As String
+        Dim processName As String
+        processName = AppPathTextBox.Text.Substring(AppPathTextBox.Text.LastIndexOf("\") + 1)
+        processName = processName.Substring(0, processName.LastIndexOf("."))
+        Return processName
+    End Function
+
+    Private Function GetProcessNameWExt() As String
+        Dim processName As String
+        processName = AppPathTextBox.Text.Substring(AppPathTextBox.Text.LastIndexOf("\") + 1)
+        Return processName
+    End Function
+
+    Private Function GetIconName() As String
+        Dim iconName As String
+        iconName = ImgPathTextBox.Text.Substring(ImgPathTextBox.Text.LastIndexOf("\") + 1)
+        Return iconName
+    End Function
+
+    Private Function GetBackgroundName() As String
+        Dim backgroundName As String
+        backgroundName = BackimageTextBox.Text.Substring(BackimageTextBox.Text.LastIndexOf("\") + 1)
+        Return backgroundName
+    End Function
+
+    Private Sub CreateStartTask()
+        Dim process As New Process()
+        Dim taskString As String
+        process.StartInfo.FileName = "C:\Windows\System32\schtasks.exe"
+        process.StartInfo.UseShellExecute = False
+        process.StartInfo.RedirectStandardOutput = True
+        process.StartInfo.RedirectStandardError = True
+        taskString = "/Create /RL HIGHEST /TN \Rainmeter\Start_" + GetProcessName() + " /TR " + """" + AppPathTextBox.Text + """" + " /SC ONCE /ST 00:01 /F"
+        process.StartInfo.Arguments = taskString
+
+        Console.WriteLine(taskString)
+        process.Start()
+
+        Dim reader As StreamReader = process.StandardOutput
+        Dim errorStream As StreamReader = process.StandardError
+        Dim output As String = reader.ReadToEnd()
+        Dim errorStr As String = errorStream.ReadToEnd()
+        Console.WriteLine("Output is " + output)
+        Console.WriteLine("Error is " + errorStr)
+
+        process.WaitForExit()
+        process.Close()
+    End Sub
+
+    Private Sub CreateStopTask()
+        Dim process As New Process()
+        Dim taskString As String
+        process.StartInfo.FileName = "C:\Windows\System32\schtasks.exe"
+        process.StartInfo.UseShellExecute = False
+        process.StartInfo.RedirectStandardOutput = True
+        process.StartInfo.RedirectStandardError = True
+        taskString = "/Create /RL HIGHEST /TN \Rainmeter\Stop_" + GetProcessName() + " /TR ""C:\Windows\System32\taskkill.exe /F /T /IM " + GetProcessNameWExt() + """ /SC ONCE /ST 00:01 /F"
+        process.StartInfo.Arguments = taskString
+
+        Console.WriteLine(taskString)
+        process.Start()
+
+        Dim reader As StreamReader = process.StandardOutput
+        Dim errorStream As StreamReader = process.StandardError
+        Dim output As String = reader.ReadToEnd()
+        Dim errorStr As String = errorStream.ReadToEnd()
+        Console.WriteLine("Output is " + output)
+        Console.WriteLine("Error is " + errorStr)
+
+        process.WaitForExit()
+        process.Close()
+    End Sub
+
+    Private Sub BuildIniFile()
+        Dim baseFileReader As String
+        Dim finalFileText As String
+        Dim RainmeterLocation As String
+        RainmeterLocation = My.Computer.FileSystem.SpecialDirectories.MyDocuments + "\Rainmeter"
+
+        If MonitorCheck.Checked = True Then
+            baseFileReader = My.Resources.baseMonitor
+        Else
+            baseFileReader = My.Resources.baseNoMonitor
+        End If
+
+        finalFileText = baseFileReader
+
+        finalFileText = finalFileText.Replace("@ContextMenuAddOn@", "")
+
+        If ExitAbility.Checked = True Then
+            Dim exitFileReader As String
+            exitFileReader = My.Resources.KillTask
+            finalFileText = finalFileText.Replace("@KillTask@", exitFileReader)
+        Else
+            finalFileText = finalFileText.Replace("@KillTask@", "")
+        End If
+
+        If BackgroundCheck.Checked = True Then
+            Dim backgroundFileReader As String
+            backgroundFileReader = My.Resources.ShowBackground
+            finalFileText = finalFileText.Replace("@ShowBackground@", backgroundFileReader)
+
+            Dim backgroundTemplateReader As String
+            backgroundTemplateReader = My.Resources.background1
+            backgroundTemplateReader = backgroundTemplateReader.Replace("@Background@", GetBackgroundName())
+            Dim newBackgroundFile As System.IO.StreamWriter
+            My.Computer.FileSystem.CreateDirectory(RainmeterLocation + "\Skins\RSC\" + GetProcessName() + "\background")
+            newBackgroundFile = My.Computer.FileSystem.OpenTextFileWriter(RainmeterLocation + "\Skins\RSC\" + GetProcessName() + "\background\background.ini", False)
+            newBackgroundFile.WriteLine(backgroundTemplateReader)
+            newBackgroundFile.Close()
+
+            My.Computer.FileSystem.CopyFile(
+                BackimageTextBox.Text,
+                RainmeterLocation + "\Skins\RSC\" + GetProcessName() + "\background\" + GetBackgroundName(),
+                Microsoft.VisualBasic.FileIO.UIOption.AllDialogs,
+                Microsoft.VisualBasic.FileIO.UICancelOption.DoNothing)
+
+        Else
+            finalFileText = finalFileText.Replace("@ShowBackground@", "")
+        End If
+
+        finalFileText = finalFileText.Replace("@ApplicationPath@", AppPathTextBox.Text.Substring(0, AppPathTextBox.Text.LastIndexOf("\")))
+        finalFileText = finalFileText.Replace("@SkinPath@", "RSC\" + GetProcessName())
+        finalFileText = finalFileText.Replace("@StopTaskName@", "\Rainmeter\Stop_" + GetProcessName())
+        finalFileText = finalFileText.Replace("@StartTaskName@", "\Rainmeter\Start_" + GetProcessName())
+        finalFileText = finalFileText.Replace("@ProcessName@", GetProcessName())
+        finalFileText = finalFileText.Replace("@ProcessNameWExt@", GetProcessNameWExt())
+        finalFileText = finalFileText.Replace("@Icon@", GetIconName())
+
+        Dim newIniFile As System.IO.StreamWriter
+        My.Computer.FileSystem.CreateDirectory(RainmeterLocation + "\Skins\RSC\" + GetProcessName())
+        newIniFile = My.Computer.FileSystem.OpenTextFileWriter(RainmeterLocation + "\Skins\RSC\" + GetProcessName() + "\" + GetProcessName() + ".ini", False)
+        newIniFile.WriteLine(finalFileText)
+        newIniFile.Close()
+
+        My.Computer.FileSystem.CopyFile(
+            ImgPathTextBox.Text,
+            RainmeterLocation + "\Skins\RSC\" + GetProcessName() + "\" + GetIconName(),
+            Microsoft.VisualBasic.FileIO.UIOption.AllDialogs,
+            Microsoft.VisualBasic.FileIO.UICancelOption.DoNothing)
+
+    End Sub
+
+    Private Sub ShortcutCreator_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For Each argument As String In My.Application.CommandLineArgs
+            AppPathTextBox.Text = argument
+        Next
+
+    End Sub
+End Class
